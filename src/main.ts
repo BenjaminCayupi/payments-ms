2b8cae254d0d8f40a55c1bef3a635494d7559be1
benjaminCayupi <benjamin.cayupi@gmail.com>
2024-10-18 18:01:09 -0300

    Adds env config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { envs } from './config';
 
 async function bootstrap() {
+  const logger = new Logger('Payments MS');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api');
+
+  await app.listen(envs.port);
+
+  logger.log(`Running on port ${envs.port}`);
 }
 bootstrap();
